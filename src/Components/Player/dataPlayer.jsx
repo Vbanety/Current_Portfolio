e289f23a56f615df783150bd475894137203526b
Vinicius Batista <vbanetyy@gmail.com>
2022-07-05 13:08:53 -0300

    update name audio

--- src/Components/Player/dataPlayer.jsx
+++ src/Components/Player/dataPlayer.jsx
@@ -9,28 +9,28 @@ export const DataPlayer = [
     title: 'Jump',
     author: 'Van Halen',
     picture: vanHalenPicure,
-    player: '/public/assets/sounds/VanHalenJump.mp4'
+    player: '/public/assets/sounds/audio04.mp4'
   },
 
   {
     title: "Don't Stop Believin",
     author: 'Journey',
     picture: journeyPicture,
-    player: "/public/assets/sounds/JourneyDontStopBelievin.mp4"
+    player: "/public/assets/sounds/audio03.mp4"
   },
 
   {
     title: "Summer of '69",
     author: "Bryan Adams",
     picture: bryanPicture,
-    player: "/public/assets/sounds/BryanAdams Summerof69.mp4"
+    player: "/public/assets/sounds/audio01.mp4"
   },
 
   {
     title: "Girls Just Want",
     author: "Cyndi Louper",
     picture: cyndiPicure,
-    player: "/public/assets/sounds/GirlsJustWannaHaveFun.mp4"
+    player: "/public/assets/sounds/audio02.mp4"
   }
 
 ]
